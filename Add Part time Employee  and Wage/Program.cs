b48072e0cf2098f17f7b0b77af7a6318a5e06f57
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-12-03 19:05:36 +0530

    Add Part Employee and Calculate Wage

---
+++ Add Part time Employee  and Wage/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Add_Part_time_Employee__and_Wage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Full_Time = 1;
+            int Part_Time = 2;
+            int Emp_Per_Hours = 20;
+            int empHrs, empWage;
+            Random random = new Random();
+            int check = random.Next(0, 3);
+            if (check == Full_Time)
+            {
+                empHrs = 8;
+            }
+            else if (check == Part_Time)
+            {
+                empHrs = 4;
+            }
+            else
+            {
+                empHrs = 0;
+            }
+            empWage = empHrs * Emp_Per_Hours;
+            Console.WriteLine("Employee Wage : " + empWage);
+        }
+    }
+}
