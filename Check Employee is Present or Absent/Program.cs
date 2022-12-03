c1544b64a0ff4f2894a0d527078a5572f20f82d4
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-12-03 18:50:56 +0530

    Check Employee Present or Absent

---
+++ Check Employee is Present or Absent/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Check_Employee_is_Present_or_Absent
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Full_Time = 1;
+            Random random= new Random();
+            int check = random.Next(0,2);
+            if (check == Full_Time)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else 
+            { 
+                Console.WriteLine("Employee is Absent");
+            }
+         
+        }
+    }
+}
