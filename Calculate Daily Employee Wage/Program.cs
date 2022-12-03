8fb7ba08da2a6bac66df271f52beb16d90ab3a30
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-12-03 18:57:31 +0530

    Calculate daily employee wage

---
+++ Calculate Daily Employee Wage/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Calculate_Daily_Employee_Wage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Full_Time = 1;
+            int Emp_Per_Hours = 20;
+            int empHrs , empWage;
+            Random random = new Random();
+            int check = random.Next(0, 2);
+            if (check == Full_Time)
+            {
+                empHrs = 8;
+            }
+            else
+            {
+                empHrs = 0;
+            }
+            empWage = empHrs * Emp_Per_Hours;
+            Console.WriteLine("Employee Wage : " + empWage);
+        }
+    }
+}
