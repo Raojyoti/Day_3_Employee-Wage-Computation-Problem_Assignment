f30a131f823580b41898f87ef009301aba7b4e0c
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-12-05 17:10:23 +0530

    Calculate wageswhen assume 100 hours

---
+++ Calculate Wages Assume 100 hours and 20 day/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Calculate_Wages_Assume_100_hours_and_20_day
+{
+    internal class Program
+    {
+        public const int Full_Time = 1;
+        public const int Part_Time = 2;
+        public const int Emp_Per_Hours = 20;
+        public const int Num_Of_Working_Days = 2;
+        public const int Max_Hrs_In_Month = 10;
+        static void Main(string[] args)
+        {
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+            totalWorkingDays++;
+            Random random = new Random();
+            int check = random.Next(0, 3);
+            while (totalEmpHrs <= Max_Hrs_In_Month && totalWorkingDays < Num_Of_Working_Days)
+            {
+                switch (check)
+                {
+                    case Full_Time:
+                        empHrs = 8;
+                        break;
+                    case Part_Time:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+               totalEmpHrs = totalEmpHrs + empHrs;
+                Console.WriteLine("Day: " + totalWorkingDays + " Emp Hrs : " + empHrs );
+            }
+            int totalEmpWage = totalEmpHrs * Emp_Per_Hours;
+            Console.WriteLine("Total Employee Wages : " + totalEmpWage);
+        }
+    }
+}
